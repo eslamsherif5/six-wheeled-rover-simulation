63a3fa4ef2043ec7f52b0f9fcb4e389e2596cdec
unknown <elmaghawri.ay@gmail.com>
2023-05-08 02:20:25 -0700

    velocity equations implementation

--- rover_diffrential.py
+++ rover_diffrential.py
@@ -3,17 +3,28 @@ import math
 from math import * 
 
 class Diffrential:
-    def __init__(self,v,omega,rho,beta,k4,k5,wheelConf,l2,l3,k7,k8):
+    def __init__(self,v,omega,k3,k3bar):
         self.v = v 
         self.omega = omega
-        self.rho = rho 
-        self.beta = beta 
-        self.k4 = k4
-        self.k5 = k5 
-        self.b = wheelConf   
-        self.l2 = l2
-        self.l3 = l3  
-        self.k7 = k7 
-        self.k8 = k8 
+        self.k3 = k3 
+        self.k3bar = k3bar 
         
-        
+    def dimensions (self):
+        # ICR redius calcuation 
+        self.r = self.omega / self.v 
+        # Dimensions for the left side of the rover 
+        self.l1 = self.r - self.k3
+        self.l35 = self.r - self.k3 - self.k3bar 
+        # Dimenstions for the right side of the rover 
+        self.l2 = self.r + self.k3
+        self.l46 = self.r - self.k3 + self.k3bar
+    
+    def velocities (self):
+        # Velcoity calcuation for the rover 
+        v1 = self.l1 * self.omega     
+        v2 = self.l2 * self.omega
+        v3 = self.l3 * self.omega
+        v4 = self.l4 * self.omega
+        v5 = self.l5 * self.omega
+        v6 = self.l6 * self.omega
+    
