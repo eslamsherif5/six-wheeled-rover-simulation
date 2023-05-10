5b376fe3a69102f48eee2bbae7e351838ebacd68
unknown <elmaghawri.ay@gmail.com>
2023-05-10 15:07:38 -0700

    Add PID as a function

--- microcontroller/velcity_control.ino
+++ microcontroller/velcity_control.ino
@@ -1,21 +1,18 @@
 unsigned long previousT  {0}; 
 unsigned long currentT {0}; 
-
 float deltaT {0};
-
 float kp {1} ;
 float ki {1} ; 
 float errorI {0};
-
 float kd {1} ; 
 float errorD {0}; 
-
 float targetVelocity {0}; 
 float currentVelocity {0}; 
 float errorVelocity  {0};
-
 float plantInput {0}; 
 
+
+float PIDController (float errorVelocity , float errorI , float errorD, float kp , float ki, float kd) ; 
 void setup()
 {
     Serial.begin(9600);
@@ -28,10 +25,17 @@ void loop()
     errorVelocity = targetVelocity - currentVelocity ;  
     errorI = errorI +  errorVelocity * deltaT ; 
     errorD =  (errorD - errorVelocity) / deltaT ; 
-    plantInput = kp * errorVelocity + ki * errorI + kd * errorD ;
 
+    plantInput = PIDController (errorVelocity,errorI,errorD,kp,ki,kd) ; 
 
     previousT = currentT ; 
-    
-    
+  
+}
+
+float PIDController (float errorVelocity , float errorI , float errorD, float kp , float ki, float kd)
+{
+    plantInput = kp * errorVelocity + ki * errorI + kd * errorD ;
+
+    return plantInput ; 
+
 }
